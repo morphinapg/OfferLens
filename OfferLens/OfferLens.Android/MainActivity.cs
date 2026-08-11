46da8b90f2b4ae1948f65b3104bb0c1b093bafdb
morphinapg <52674976+morphinapg@users.noreply.github.com>
2026-08-11 03:52:37 -0400

    Add project files.

---
+++ OfferLens/OfferLens.Android/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Avalonia;
+using Avalonia.Android;
+
+namespace OfferLens.Android;
+
+[Activity(
+    Label = "OfferLens",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
---
+++ OfferLens/OfferLens/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OfferLens.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
