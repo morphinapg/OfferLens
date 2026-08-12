ba2af26147db4894233297cc779f3b42be774ffb
morphinapg <52674976+morphinapg@users.noreply.github.com>
2026-08-12 03:49:03 -0400

    Basic UI set up

--- OfferLens/OfferLens/ViewModels/MainViewModel.cs
+++ OfferLens/OfferLens/ViewModels/MainViewModel.cs
@@ -4,6 +4,21 @@ namespace OfferLens.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
+    //[ObservableProperty]
+    //public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+
     [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    public partial decimal? PerMileTargetGood { get; set; }
+
+    [ObservableProperty]
+    public partial decimal? PerMileTargetGreat { get; set; }
+
+    [ObservableProperty]
+    public partial decimal? PerHourTargetGood { get; set; }
+
+    [ObservableProperty]
+    public partial decimal? PerHourTargetGreat { get; set; }
+
+    [ObservableProperty]
+    public partial bool UseCustomGreat { get; set; } = false;
 }
